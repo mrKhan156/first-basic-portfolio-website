689e0de9f11cddd616996cfea742c3479491171f
mrKhan156 <imrankhan772.bd@gmail.com>
2023-08-29 22:26:38 +0600

    quotes added

--- assats/style/new.js
+++ assats/style/new.js
@@ -46,3 +46,63 @@ function displayTask (){
 
 }
 displayTask();
+
+// quotes section starts
+
+let btn = document.querySelector('#new_quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.persons');
+
+const quotes  = [
+    {
+        quote:`"I am not afraid of storms, for I am learning how to sail my ship."`,
+        person:`"Louisa May Alcott"`
+    },
+    {
+        quote:`"“To choose doubt as a philosophy of life is akin to choosing immobility as a means of transportation”"`,
+        person :`"Yann Martel"`
+    },
+
+    {
+        quote:`"Tomorrow is always fresh, with no mistakes in yet."`,
+        person :`" Lucy Maud Montgomery"`
+    },
+
+    {
+        quote:`"It’s the possibility of having a dream come true that makes life interesting."`,
+        person :`"Paulo Coelho"`
+    }
+,
+    {
+        quote:`"Nothing in the world is ever completely wrong. Even a stopped clock is right twice a day."`,
+        person :`"Paulo Coelho"`
+    },
+    {
+        quote:`"I have learned to seek my happiness by limiting my desires, rather than in attempting to satisfy them."`,
+        person :`"John Stuart Mill"`
+    },
+    {
+        quote:`"Life is not a problem to be solved but a reality to be experienced."`,
+        person :`"Søren Kierkegaard"`
+    },
+    {
+        quote:`"No man ever steps in the same river twice, for it’s not the same river and he’s not the same man."`,
+        person :`"Heraclitus"`
+    },
+    {
+        quote:`"No matter what happens in life, be good to people. Being good to people is a wonderful legacy to leave behind."`,
+        person :`"Taylor Swift"`
+    },
+    {
+        quote:`"Almost everything will work again if you unplug it for a few minutes, including you."`,
+        person :` "Anne Lamott"`
+    },
+];
+
+btn.addEventListener('click',function(){
+    let random = Math.floor(Math.random() * quotes.length );
+
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].person;
+})
+// quotes section ends
