3a9bbb7bf0c5e440cc504359bc87475ee9ebfbdf
mrKhan156 <imrankhan772.bd@gmail.com>
2023-09-09 00:25:35 +0600

    stop watch added

--- assats/style/new.js
+++ assats/style/new.js
@@ -213,3 +213,78 @@ btn.addEventListener('click',function(){
     person.innerText = quotes[random].person;
 })
 // quotes section ends
+
+//stopWatch section starts
+const startStopBtn = document.querySelector('#startStopBtn');
+const resetBtn = document.querySelector('#resetBtn');
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// variables for zero
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+//variables for interval
+
+let timerInterval = null;
+let timerStatus = "stopped"
+
+//stop watch function
+function stopWatch(){
+
+
+    seconds ++
+    if(seconds/60 ===1){
+        seconds = 0;
+        minutes ++;
+        if(minutes/60 === 1){
+            minutes = 0;
+            hours ++;
+        }
+    }
+
+    if(seconds < 10){
+        leadingSeconds = "0" + seconds.toString();
+    } else{
+        leadingSeconds = seconds ;
+    }
+    if(minutes < 10){
+        leadingMinutes = "0" + minutes.toString();
+    } else{
+        leadingMinutes = minutes ;
+    }
+    if(hours<10){
+        leadingHours = "0" + hours.toString();
+    } else{
+        leadingHours = hours ;
+    }
+
+    let displayTimer = document.getElementById('timer').innerText = leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
+}
+//  
+startStopBtn.addEventListener('click',function(){
+
+if(timerStatus === "stopped"){
+
+    timerInterval= window.setInterval(stopWatch,1000);
+
+    document.getElementById('startStopBtn').innerHTML = `<i class="fa-solid fa-pause" id="pause"></i>`;
+
+    timerStatus = "started";
+} else{
+    window.clearInterval(timerInterval);
+    document.getElementById('startStopBtn').innerHTML = `<i class="fa-regular fa-circle-play" id="play"></i>`;
+    timerStatus="stopped";
+}
+});
+
+resetBtn.addEventListener('click',function(){
+window.clearInterval(timerInterval);
+seconds = 0;
+minutes = 0;
+hours = 0;
+document.getElementById('timer').innerHTML = "00:00:00";
+});
+//stopWatch section ends
